2e87901f0590632fbac835c354c5852be5eb626d
natan <natan653@gmail.com>
2022-11-26 20:59:08 +0200

    fixed login and register

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,19 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { SERVER_URL } from "../utils/serverUtil";
 import { parseJwt } from "../utils/tokenDecode";
+import "../static/css/register.css";
 
 const Login = () => {
   const [username, setUsername] = useState("guest");
   const [password, setPassword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [Invalid, setInvalid] = useState(false);
+
+  useEffect(() => {
+    localStorage.getItem("access-token") && setIsLoggedIn(true);
+  }, []);
 
   /**
    * It sends a POST request to the server with the username and password, and if the response is
@@ -16,6 +23,7 @@ const Login = () => {
    */
   const loginHandle = async (e) => {
     e.preventDefault();
+    setInvalid(false);
     let result = await axios
       .post(
         SERVER_URL + "/login/loginuser/",
@@ -25,48 +33,85 @@ const Login = () => {
         },
         { "Content-Type": "application/json" }
       )
+      .catch(() => {
+        setInvalid(true);
+      })
       .then((response) => {
         localStorage.setItem("access-token", response.data.access);
         localStorage.setItem("refresh-token", response.data.refresh);
         localStorage.setItem("user", parseJwt(response.data.access).user_id);
       });
-    window.location.href = "/mainboard";
+    setIsLoggedIn(true);
   };
 
   return (
     <div className="register-form-container">
-      <div className="register-info">
-        <form onSubmit={loginHandle}>
-          <h1>Login</h1>
-          <div>
-            <label>Username: </label>
-            <br />
-            <input
-              id="username"
-              type="text"
-              autoComplete={"false"}
-              onChange={(e) => setUsername(e.target.value)}
-            ></input>
+      {isLoggedIn ? (
+        <div className="after-submit">
+          <div className="after-submit-message">
+            <span>
+              <h2>Logged in successfuly</h2>
+            </span>
           </div>
-          <div>
-            <label>Password: </label>
-            <br />
-            <input
-              id="password"
-              type={"password"}
-              autoComplete={"false"}
-              onChange={(e) => setPassword(e.target.value)}
-            ></input>
-          </div>
-          <p>
-            Dont have an account?{" "}
-            <NavLink className="register-or-login" to="/register">
-              Register
+          <div className="after-submit-links">
+            <h2>Where would you like to visit ?</h2>
+            <NavLink className="navigation-link" to="/mainboard">
+              Store
             </NavLink>
-          </p>
-          <button>Log in</button>
-        </form>
-      </div>
+            <NavLink className="navigation-link" to="/profile">
+              Your profile
+            </NavLink>
+            <NavLink className="navigation-link" to="/cart">
+              Your cart
+            </NavLink>
+            <NavLink className="navigation-link" to="/wishlist">
+              Your wishlist
+            </NavLink>
+          </div>
+        </div>
+      ) : (
+        <div className="register-info">
+          <form onSubmit={loginHandle}>
+            <h1>Login</h1>
+            <div>
+              <label>Username: </label>
+              <br />
+              <input
+                required
+                id="username"
+                type="text"
+                autoComplete={"false"}
+                onChange={(e) => setUsername(e.target.value)}
+              ></input>
+            </div>
+            <div>
+              <label>Password: </label>
+              <br />
+              <input
+                required
+                id="password"
+                type={"password"}
+                autoComplete={"false"}
+                onChange={(e) => setPassword(e.target.value)}
+              ></input>
+            </div>
+            {Invalid && (
+              <div className="modal">
+                <div className="modal-content">
+                  <span>One of the credentials is incorrect</span>
+                </div>
+              </div>
+            )}
+            <p>
+              Dont have an account?{" "}
+              <NavLink className="register-or-login" to="/register">
+                Register
+              </NavLink>
+            </p>
+            <button>Log in</button>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Register.js
+++ src/components/Register.js
@@ -10,93 +10,122 @@ const Register = () => {
   const [lastName, setLastName] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
+  const [isRegisterd, setIsRegisterd] = useState(false);
 
   /**
    * When the user clicks the register button, the function will send a post request to the server with
-   * the user's information, and then redirect the user to the login page.
+   * the user's information.
    */
-  const registerHandle = async () => {
-    const response = await axios.post(SERVER_URL + "/register/register/", {
-      username: username,
-      first_name: firstName,
-      last_name: lastName,
-      email: email,
-      password: password,
-    });
-    window.location.href = "/login";
+  const registerHandle = async (e) => {
+    e.preventDefault();
+    const response = await axios
+      .post(SERVER_URL + "/register/register/", {
+        username: username,
+        first_name: firstName,
+        last_name: lastName,
+        email: email,
+        password: password,
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .then((res) => {
+        console.log(res);
+      });
+    setIsRegisterd(true);
   };
 
   return (
     <div className="register-form-container">
-      <div className="register-info">
-        <form onSubmit={registerHandle}>
-          <h1>
-            <span>Sign up</span>
-          </h1>
-          <div>
-            <label>First name: </label>
-            <br />
-            <input
-              id="first_name"
-              autoComplete={"false"}
-              type="text"
-              onChange={(e) => setFirstName(e.target.value)}
-            ></input>
+      {isRegisterd ? (
+        <div className="after-submit">
+          <div className="after-submit-message">
+            <span>
+              <h2>Successfuly signed up !</h2>
+            </span>
           </div>
-          <div>
-            <label>Last name: </label>
-            <br />
-            <input
-              id="last_name"
-              type="text"
-              autoComplete={"false"}
-              onChange={(e) => setLastName(e.target.value)}
-            ></input>
+          <div className="after-submit-links">
+            <h2>
+              Now, <br /> lets log you in.
+            </h2>
+            <button>
+              <NavLink className="navigation-link" to="/login">
+                Click here
+              </NavLink>
+            </button>
           </div>
-          <div>
-            <label>Username: </label>
-            <br />
-            <input
-              required
-              id="username"
-              type="text"
-              autoComplete={"false"}
-              onChange={(e) => setUsername(e.target.value)}
-            ></input>
-          </div>
-          <div>
-            <label>Email: </label>
-            <br />
-            <input
-              required
-              id="email"
-              type={"email"}
-              autoComplete={"false"}
-              onChange={(e) => setEmail(e.target.value)}
-            ></input>
-          </div>
-          <div>
-            <label>Password: </label>
-            <br />
-            <input
-              required
-              id="password"
-              type={"password"}
-              autoComplete={"false"}
-              onChange={(e) => setPassword(e.target.value)}
-            ></input>
-          </div>
-          <p>
-            Already have an account?{" "}
-            <NavLink className="register-or-login" to="/login">
-              Login
-            </NavLink>
-          </p>
-          <div className="register-submit">
-            <button type="submit">Sign me up</button>
-          </div>
-        </form>
-      </div>
+        </div>
+      ) : (
+        <div className="register-info">
+          <form onSubmit={registerHandle}>
+            <h1>
+              <span>Sign up</span>
+            </h1>
+            <div>
+              <label>First name: </label>
+              <br />
+              <input
+                id="first_name"
+                autoComplete={"false"}
+                type="text"
+                onChange={(e) => setFirstName(e.target.value)}
+              ></input>
+            </div>
+            <div>
+              <label>Last name: </label>
+              <br />
+              <input
+                id="last_name"
+                type="text"
+                autoComplete={"false"}
+                onChange={(e) => setLastName(e.target.value)}
+              ></input>
+            </div>
+            <div>
+              <label>Username: </label>
+              <br />
+              <input
+                required
+                id="username"
+                type="text"
+                autoComplete={"false"}
+                onChange={(e) => setUsername(e.target.value)}
+              ></input>
+            </div>
+            <div>
+              <label>Email: </label>
+              <br />
+              <input
+                required
+                id="email"
+                type={"email"}
+                autoComplete={"false"}
+                onChange={(e) => setEmail(e.target.value)}
+              ></input>
+            </div>
+            <div>
+              <label>Password: </label>
+              <br />
+              <input
+                required
+                id="password"
+                type={"password"}
+                autoComplete={"false"}
+                onChange={(e) => setPassword(e.target.value)}
+              ></input>
+            </div>
+            <p>
+              Already have an account?{" "}
+              <NavLink className="register-or-login" to="/login">
+                Login
+              </NavLink>
+            </p>
+            <div className="register-submit">
+              <button type="submit">Sign me up</button>
+            </div>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
